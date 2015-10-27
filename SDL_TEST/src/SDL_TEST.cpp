881562e3fb01b3f5be533c3bf6f24fca9c865aae
AxjB <alexej3535@web.de>
2015-10-27 10:58:56 +0100

    eclipse project

---
+++ SDL_TEST/src/SDL_TEST.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : SDL_TEST.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#define NOMINMAX
+
+#include <iostream>
+#include <windows.h>
+#include "SDL2/SDL.h"
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int status = SDL_Init(SDL_INIT_EVERYTHING);
+cout<<status<<endl;
+
+
+	return 0;
+}
